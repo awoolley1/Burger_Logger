7ebc32a5e939a9573447ecb6347e3bd0d3875162
Local Al <alexander.s.woolley@gmail.com>
2021-02-11 16:10:30 -0700

    db seeded

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Habanero Bacon', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Triple Ranch Dipper', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Soggy', true);
