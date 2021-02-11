88e3b1a4d86e602c2d7c7b011e9bfa6d8aac1184
Local Al <alexander.s.woolley@gmail.com>
2021-02-11 13:21:53 -0700

    orm.js

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql');
+require('dotenv').config()
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: process.env.PASSWORD,
+  database: 'cat_db',
+});
+
+
+connection.connect((err) => {
+  if (err) {
+    console.error(`error connecting: ${err.stack}`);
+    return;
+  }
+  console.log(`connected as id ${connection.threadId}`);
+});
+
+
+module.exports = connection;
