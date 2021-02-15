75cb42ae14d1d0779437aa9bc71bc5918d86159f
Local Al <alexander.s.woolley@gmail.com>
2021-02-15 13:07:11 -0700

    add button

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,3 +1,48 @@
 // link to main handlebars 
 // + add event listener for button here
 //ajax call to the put route that I create
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    if (event) {
+      console.info("DOM loaded");
+    } 
+
+  // UPDATE
+   // const changeToDevouredBtn = document.querySelectorAll(".change-devoured"); 
+
+
+  // CREATE
+  const createBurgerBtn = document.getElementById('create-form');
+
+  if (createBurgerBtn) {
+    createBurgerBtn.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      const newBurger = {
+        name: document.getElementById('ca').value.trim(),
+      };
+
+      // Send POST request to create a new quote
+      fetch('/api/burgers', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+
+        // make sure to serialize the JSON body
+        body: JSON.stringify(newCat),
+      }).then(() => {
+        // Empty the form
+        document.getElementById('ca').value = '';
+
+        // Reload the page so the user can see the new quote
+        console.log('Created a new cat!');
+        location.reload();
+      });
+    });
+  }
+
+});
+
+
